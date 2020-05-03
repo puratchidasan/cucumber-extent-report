735dcec57f4741a50849c79636cec51fb4239ba6
Puratchidasan Munusami <puratchidasan@gmail.com>
2020-05-03 22:09:25 +0200

    Removed unused imports from Java files

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -1,6 +1,5 @@
 package runner;
 
-import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
--- src/test/java/selenium/NullWebElement.java
+++ src/test/java/selenium/NullWebElement.java
@@ -5,9 +5,7 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Point;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 
 public class NullWebElement implements WebElement {
